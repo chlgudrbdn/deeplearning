a484c5de3035e849dcfe3203df2fc36a5b28d75f
hyoung-gyu <chlgudrbdn@gmail.com>
2018-07-26 19:03:40 +0900

    from <orderEntry type="jdk" jdkName="Python 3.5" jdkType="Python SDK" /> to <orderEntry type="jdk" jdkName="Python 3.5 (deeplearning)" jdkType="Python SDK" />

--- deep_class/test.py
+++ deep_class/test.py
@@ -1,38 +1,9 @@
-# import tensorflow as tf
-#
-# a = tf.random_uniform([2,3],seed=42)
-#
-# sess = tf.Session()
-# sess.run( tf.global_variables_initializer() )
-#
-# for i in range(1):
-#     print("a=", sess.run(a))
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from pandas import read_csv
 
-from keras.datasets import mnist
-from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
-from keras.callbacks import ModelCheckpoint,EarlyStopping
+# load data
+def parse(x):
+	return datetime.strptime(x, '%Y%m%d')
 
-import matplotlib.pyplot as plt
-import numpy
-import os
-import tensorflow as tf
-
-# seed 값 설정
-seed = 0
-numpy.random.seed(seed)
-tf.set_random_seed(seed)
-
-# 데이터 불러오기
-
-(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-print(X_test.shape[0])
-a = numpy.arange(7840000).reshape(10000, 28, 28, 1)
-print(a)
-
-
-# X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32') / 255  #좀 독특해보이는 reshape인데
-# X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32') / 255
-# Y_train = np_utils.to_categorical(Y_train)
-# Y_test = np_utils.to_categorical(Y_test)
+dataset = read_csv('date_And_ironorePrice-forecast.csv',  parse_dates = ['X'], date_parser=parse)
--- deep_code/multivariate-time-series-forecasting-lstms-keras.py
+++ deep_code/multivariate-time-series-forecasting-lstms-keras.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
 from math import sqrt
 from numpy import concatenate
 from matplotlib import pyplot
@@ -10,39 +12,72 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-import os
+
+
+# load data
+def parse(x):
+    return datetime.strptime(x, '%Y %m %d %H')
+
+
+dataset = read_csv('raw.csv', parse_dates=[['year', 'month', 'day', 'hour']], index_col=0, date_parser=parse)
+dataset.drop('No', axis=1, inplace=True)
+# manually specify column names
+dataset.columns = ['pollution', 'dew', 'temp', 'press', 'wnd_dir', 'wnd_spd', 'snow', 'rain']
+dataset.index.name = 'date'
+# mark all NA values with 0
+dataset['pollution'].fillna(0, inplace=True)
+# drop the first 24 hours
+dataset = dataset[24:]
+# summarize first 5 rows
+print(dataset.head(5))
+# save to file
+dataset.to_csv('pollution.csv')
+
+# load dataset
+dataset = read_csv('pollution.csv', header=0, index_col=0)
+values = dataset.values
+# specify columns to plot
+groups = [0, 1, 2, 3, 5, 6, 7]
+i = 1
+# plot each column
+pyplot.figure()
+for group in groups:
+    pyplot.subplot(len(groups), 1, i)
+    pyplot.plot(values[:, group])
+    pyplot.title(dataset.columns[group], y=0.5, loc='right')
+    i += 1
+pyplot.show()
 
 
 # convert series to supervised learning
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-    n_vars = 1 if type(data) is list else data.shape[1]
+    n_vars = 1 if type(data) is list else data.shape[1] # 딱히 data의 type이 리스트가 아니면 변수 개수 추출. 리스트라면 1.
     df = DataFrame(data)
     cols, names = list(), list()
     # input sequence (t-n, ... t-1)
-    for i in range(n_in, 0, -1):
-        cols.append(df.shift(i))
-        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    for i in range(n_in, 0, -1): # 예상에 쓸 t이전 데이터들.
+        cols.append(df.shift(i)) # 일단 사용된 코드에선 1이라고 n_in을 정해서 i는 1뿐. 실제론 더 많은 범위의 데이터를 이용할테니 t-1, t-2...가 t시점의 종속변수에 대응.
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)] #
     # forecast sequence (t, t+1, ... t+n)
-    for i in range(0, n_out):
+    for i in range(0, n_out): # t +a 앞으로 예상할 범위. 물론 그 예상한 속성들로 종속변수를 계산해야할테니 속성개수만큼 t+a를 예상.
         cols.append(df.shift(-i))
         if i == 0:
             names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
         else:
             names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
     # put it all together
-    agg = concat(cols, axis=1)
-    agg.columns = names
+    agg = concat(cols, axis=1) # axis=1은 좌우로 합치는 의미.
+    agg.columns = names # 이름 부여. (t-1들)
     # drop rows with NaN values
-    if dropnan:
-        agg.dropna(inplace=True)
+    if dropnan:# True로만 되긴 한다.
+        agg.dropna(inplace=True) # dataframe에서 dropna에 inplace=True이면 NA있는 행은 모두 제거.
     return agg
 
 
-filename = os.getcwd() + '\pollution.csv'
 # load dataset
-dataset = read_csv(filename, header=0, index_col=0)
+dataset = read_csv('pollution.csv', header=0, index_col=0)
 values = dataset.values
-# integer encode direction
+# integer encode direction # factor라 볼 수 있는 데이터는 걍 숫자로 바꿈.
 encoder = LabelEncoder()
 values[:, 4] = encoder.fit_transform(values[:, 4])
 # ensure all data is float
@@ -51,10 +86,10 @@ values = values.astype('float32')
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
 # frame as supervised learning
-reframed = series_to_supervised(scaled, 1, 1)
+reframed = series_to_supervised(scaled, 1, 1) # 각 변수들이 어떻게 시간차나는지를 반영할 용량으로 함.
 # drop columns we don't want to predict
 reframed.drop(reframed.columns[[9, 10, 11, 12, 13, 14, 15]], axis=1, inplace=True)
-print(reframed.head())
+print(reframed.head())  # scaled 된 것들.
 
 # split into train and test sets
 values = reframed.values
@@ -75,7 +110,8 @@ model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
 # fit network
-history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2,
+                    shuffle=False)
 # plot history
 pyplot.plot(history.history['loss'], label='train')
 pyplot.plot(history.history['val_loss'], label='test')
