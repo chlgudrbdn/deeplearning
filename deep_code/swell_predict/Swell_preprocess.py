11a037d9efa997dc8c3c2723533ca5e713ab1f25
research_choe <chlgudrbdn@gmail.com>
2018-08-07 18:32:29 +0900

    finish. after all project should be based on these files

--- deep_code/swell_predict/Swell_preprocess.py
+++ deep_code/swell_predict/Swell_preprocess.py
@@ -9,21 +9,30 @@ from sklearn.feature_extraction.text import CountVectorizer
 from collections import OrderedDict, defaultdict
 from keras.preprocessing import sequence
 
-# filename = os.getcwd() + '\swell_for_preprocess.csv'
-filename = os.getcwd() + '\dataset\swell_for_preprocess.csv'
+filename = os.getcwd() + '\swell_for_preprocess.csv'
+# filename = os.getcwd() + '\dataset\swell_for_preprocess.csv'
 df = pd.read_csv(filename)
 dataset = df.values
 # dataset = dataset.astype('float32')
 # normalize the dataset
 # scaler = MinMaxScaler(feature_range=(0, 1))
+WallPo_swell = pd.read_csv('(과제2) 관련. 월포 파고부이 데이터(15~17).csv')
+WallPo_swell = pd.read_csv('(과제2) 관련. 월포 파고부이 데이터(15~17).csv')
 
-zeroMatrix = np.zeros(((365*4)+1, 24))  # +1은 2016년 윤달
+zeroMatrix = np.zeros(((365*4)+1, 24))  # +1은 2016년 윤달. 7시부터 다음날 7시까지 24칸. 2014년 부터 2017년 자료를 대상.
+# 일단 오버샘플링도 고려할 필요는 있다만 그건 나중에 생각해보자.
+df['좌']
+df['우']
+
+# 30분 짜리도 있긴 하지만 딱히 정밀도에 영향을 주거나 할 것 같진 않다.
 
 
-
-df = pd.DataFrame({'$a':[1,2], '$b': [10,20]})
-df.columns = ['a', 'b']
-df
+zeroDF = pd.DataFrame(data=zeroMatrix)
+zeroDF.columns = ["07:00~08:00", "08:00~09:00", "09:00~10:00", "10:00~11:00", "11:00~12:00", "12:00~13:00",
+                  "13:00~14:00", "14:00~15:00",	"15:00~16:00", "16:00~17:00", "17:00~18:00", "18:00~~19:00",
+                  "19:00~20:00", "20:00~21:00", "21:00~22:00", "22:00~23:00", "23:00~24:00", "00:00~01:00",
+                  "01:00~02:00", "02:00~03:00", "03:00~04:00", "04:00~05:00", "05:00~06:00", "06:00~07:00"]
+zeroDF
 
 for menus in menu:
     menu_elem = menus.split(",")
